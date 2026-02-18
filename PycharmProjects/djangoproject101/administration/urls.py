4334cda523eae074b792e88d08cf0f933855a307
gacharahenry8-design <gacharahenry8@gmail.com>
2026-02-18 13:47:47 +0300

    first commit

---
+++ PycharmProjects/djangoproject101/administration/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for morgan_school project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/',views.index,name='index'),
+    path('portfolio-details/',views.portfolio-details,name='portfolio-details'),
+    path('services-details/',views.services-details,name='services-details'),
+    path('starterpage/',views.member_details,name='member_details'),
+]
